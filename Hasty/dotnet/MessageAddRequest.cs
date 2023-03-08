7b89afca29c037c865dd5d338a2d1fefb73abbd8
Christopher Doupis <57472851+starfiresd@users.noreply.github.com>
2023-03-08 10:05:04 -0800

    Update MessageAddRequest.cs

--- Hasty/dotnet/MessageAddRequest.cs
+++ Hasty/dotnet/MessageAddRequest.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Sabio.Models.Requests.Messages
+namespace Hasty.Models.Requests.Messages
 {
     public class MessageAddRequest
     {
