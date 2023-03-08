a6b95f84edb97bc8f37f8be29c6c9d12be337fb4
Christopher Doupis <57472851+starfiresd@users.noreply.github.com>
2023-03-08 10:07:06 -0800

    Update MessageUpdateRequest.cs

--- Hasty/dotnet/MessageUpdateRequest.cs
+++ Hasty/dotnet/MessageUpdateRequest.cs
@@ -1,11 +1,11 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Sabio.Models.Requests.Messages
+namespace Hasty.Models.Requests.Messages
 {
     public class MessageUpdateRequest : MessageAddRequest, IModelIdentifier
     {
