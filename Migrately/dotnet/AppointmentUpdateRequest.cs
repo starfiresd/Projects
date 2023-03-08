493f475607b07b4193dcda312cecb3e41f35a63a
Christopher Doupis <57472851+starfiresd@users.noreply.github.com>
2023-03-08 10:10:35 -0800

    Update AppointmentUpdateRequest.cs

--- Migrately/dotnet/AppointmentUpdateRequest.cs
+++ Migrately/dotnet/AppointmentUpdateRequest.cs
@@ -1,10 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Sabio.Models.Requests.Appointments
+namespace Migrately.Models.Requests.Appointments
 {
     public class AppointmentUpdateRequest : AppointmentAddRequest, IModelIdentifier
     {
